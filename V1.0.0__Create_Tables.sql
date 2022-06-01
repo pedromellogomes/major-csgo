454860f3bb68a4366e273b8825976e23ed211cd8
Pedro Mello <pedromellogomes@gmail.com.br>
2022-06-01 15:10:06 -0300

    Linting & add FK rodada.id_equipe_vencedora

--- V1.0.0__Create_Tables.sql
+++ V1.0.0__Create_Tables.sql
@@ -1,87 +1,98 @@
-create table majorcsgo(
-	id serial not null,
-	edicao integer not null,
-	local varchar(250),
-	data date not null,
-	
-	primary key (id)
+create table majorcsgo
+(
+    id     serial  not null,
+    edicao integer not null,
+    local  varchar(250),
+    data   date    not null,
+
+    primary key (id)
 );
 
-create table mapa(
-	id serial not null,
-	nome varchar(250),
-	
-	primary key (id)
+create table mapa
+(
+    id   serial not null,
+    nome varchar(250),
+
+    primary key (id)
 );
 
-create table equipe(
-	id serial not null,
-	nome varchar(100),
-	
-	primary key (id)
+create table equipe
+(
+    id   serial not null,
+    nome varchar(100),
+
+    primary key (id)
 );
 
-create table historico_participacao(
-	id serial not null,
-	id_major integer not null,
-	id_equipe integer not null,
-	
-	primary key (id),
-	foreign key (id_major) references majorcsgo(id),
-	foreign key (id_equipe) references equipe(id)
+create table historico_participacao
+(
+    id        serial  not null,
+    id_major  integer not null,
+    id_equipe integer not null,
+
+    primary key (id),
+    foreign key (id_major) references majorcsgo (id),
+    foreign key (id_equipe) references equipe (id)
 );
 
-create table jogador(
-	id serial not null,
-	id_equipe integer not null,
-	nome varchar(100),
-	
-	primary key (id),
-	foreign key (id_equipe) references equipe(id)
-);
+create table jogador
+(
+    id        serial  not null,
+    id_equipe integer not null,
+    nome      varchar(100),
 
-/**
- * RESTRIÇÕES
- * Partida ocorre em 3 turnos
- */
-create table partida(
-	id serial not null,
-	id_major integer not null,
-	id_equipe_mandante integer not null,
-	id_equipe_visitante integer not null,
-	id_equipe_ganhadora integer not null,
-	/* TODO: adicionar campo com placar final */
-	
-	primary key (id),
-	foreign key (id_major) references majorcsgo(id),
-	foreign key (id_equipe_mandante) references equipe(id),
-	foreign key (id_equipe_visitante) references equipe(id),
-	foreign key (id_equipe_ganhadora) references equipe(id)
-);
-
-/**
- * RESTRIÇÕES
- * Somente 3 turnos podem fazer referência à mesma partida
- */
-create table turno(
-	id serial not null,
-	id_mapa integer not null,
-	id_equipe_vencedor integer not null,
-	
-	primary key (id),
-	foreign key (id_mapa) references mapa(id),
-	foreign key (id_equipe_vencedor) references equipe(id)
+    primary key (id),
+    foreign key (id_equipe) references equipe (id)
 );
 
 /**
  * RESTRIÇÕES
  * Partida ocorre em 3 turnos
  */
-create table rodada(
-	id serial not null,
-	id_turno integer not null,
-		
-	primary key (id),
-	foreign key (id_turno) references turno(id)
+create table partida
+(
+    id                  serial  not null,
+    id_major            integer not null,
+    id_equipe_mandante  integer not null,
+    id_equipe_visitante integer not null,
+    id_equipe_ganhadora integer not null,
+    /* TODO: adicionar campo com placar final */
+
+    primary key (id),
+    foreign key (id_major) references majorcsgo (id),
+    foreign key (id_equipe_mandante) references equipe (id),
+    foreign key (id_equipe_visitante) references equipe (id),
+    foreign key (id_equipe_ganhadora) references equipe (id)
 );
 
+/**
+ * RESTRIÇÕES DE NEGÓCIO
+ * São 3 turnos por partida.
+ * Vence aquele que ganhar 2.
+ */
+create table turno
+(
+    id                  serial  not null,
+    id_mapa             integer not null,
+    id_equipe_vencedora integer not null,
+
+    primary key (id),
+    foreign key (id_mapa) references mapa (id),
+    foreign key (id_equipe_vencedor) references equipe (id)
+);
+
+/**
+ * RESTRIÇÕES DE NEGÓCIO
+ * Ganha o turno quem vencer 16 rodadas primeiro
+ * A diferença entre placar_mandando e placar_visitante deve ser de 2.
+ */
+create table rodada
+(
+    id                  serial  not null,
+    id_turno            integer not null,
+    id_equipe_vencedora integer not null,
+
+    primary key (id),
+    foreign key (id_turno) references turno (id),
+    foreign key (id_equipe_vencedora) references equipe (id)
+);
