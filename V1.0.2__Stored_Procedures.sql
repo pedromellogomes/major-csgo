ef8751fe150d287589c502b7e5e2fb0119a52f2a
Pedro Mello <pedromellogomes@gmail.com.br>
2022-06-12 22:33:28 -0300

    Add function

--- V1.0.2__Stored_Procedures.sql
+++ V1.0.2__Stored_Procedures.sql
@@ -92,19 +92,53 @@ $$
     LANGUAGE plpgsql;
 
 /**
-  6. Dado turno X, retorna verdadeiro se:
+  6. Dado turno X, insere novo registro se:
     ( quantidade_rodadas >= 31 )
         &&
     ( vitoria_visitante - vitoria_mandante >= [2, -2] )
  */
-CREATE OR REPLACE FUNCTION valida_turno(
-    idTurno int
-) RETURNS boolean AS
+CREATE OR REPLACE FUNCTION valida_se_turno_terminou_antes_de_inserir_nova_rodada()
+  RETURNS TRIGGER
+  LANGUAGE PLPGSQL
+  AS
 $$
 BEGIN
-    RETURN (SELECT diferenca_de_vitorias_entre_equipe_por_turno(idTurno) >= 2)
-        AND
-           (SELECT conta_rodadas_por_turno(idTurno) >= 31);
+    RAISE NOTICE 'Valida se turno já alcançou total de rodadas permitido.';
+
+    IF conta_rodadas_por_turno(NEW.id_turno) >= 31 AND
+       diferenca_de_vitorias_entre_equipe_por_turno(NEW.id_turno) >= 2 THEN
+        RAISE NOTICE 'Já ocorreram 31 ou mais rodadas e a diferença de rodadas vencidas pelas equipes é 2 ou mais';
+        RETURN NULL;
+    ELSE
+        RAISE NOTICE 'Ainda não foram jogadas as 31 rodadas mínimas ou a diferença de rodadas vencidas pelas equipes não é 2 ou mais';
+        INSERT INTO rodada (id_turno, id_equipe_vencedora) VALUES (NEW.id_turno, NEW.id_equipe_vencedora);
+        RETURN NEW;
+    END IF;
 END;
+$$;
+
+/**
+  7. Dado partida X, insere novo registro se:
+    ( quantidade_turnos < 3 )
+        ||
+    ( COUNT(id_equipe_vencedora) < 2 )
+ */
+CREATE OR REPLACE FUNCTION valida_se_partida_terminou_antes_de_inserir_nova_turno()
+  RETURNS TRIGGER
+  LANGUAGE PLPGSQL
+  AS
 $$
-    LANGUAGE plpgsql;
+BEGIN
+    RAISE NOTICE 'Valida se partida já alcançou total de turnos permitido.';
+
+    IF conta_turnos_por_partida(NEW.id_partida) >= 3 OR
+       alguma_equipe_venceu_dois_turnos_na_mesma_partida(NEW.id_partida) THEN
+        RAISE NOTICE 'Já ocorreram três turnos para a mesma partida ou uma equipe já venceu duas partidas';
+        RETURN NULL;
+    ELSE
+        RAISE NOTICE 'Ainda não ocorreram três turnos para a mesma partida ou uma equipe ainda não venceu duas partidas';
+        INSERT INTO turno (id_partida, id_mapa, id_equipe_vencedora) VALUES (NEW.id_partida, NEW.id_mapa, NEW.id_equipe_vencedora);
+        RETURN NEW;
+    END IF;
+END;
+$$;
